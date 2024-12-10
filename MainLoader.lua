5dffdefe3d13480fcfb15522b0e2ff428500a3c9
Jung Ganmyeon <148054296+junggamyeon@users.noreply.github.com>
2024-12-10 12:25:21 +0700

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,10 +1,4 @@
 if game.PlaceId == 84188796720288 then
     print ("Anime Realm")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/junggamyeon/MyScript/refs/heads/main/AnimeRealm.lua"))()
-repeat
-    wait()
-until game:IsLoaded()
-wait()
-for _, v in next, getconnections(game:GetService("Players").LocalPlayer.Idled) do
-    v:Disable()
 end
