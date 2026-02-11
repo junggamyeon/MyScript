c930d5cae2c3bd9d7c8a10d9be90b1cba034a3e1
Jung Ganmyeon <148054296+junggamyeon@users.noreply.github.com>
2026-02-11 17:38:37 +0700

    Update obfuscated_message.lua

--- obfuscated_message.lua
+++ obfuscated_message.lua
@@ -1,2 +1,1 @@
-getgenv().SCRIPT_KEY = "KEYLESS"
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/3edb64162132bba1d62602acf9f106d1d2ec2d1b53f875e6b5b17290b4ade0f1/download"))()
